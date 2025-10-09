4aba2f2225947a4e1f27ebae084658b92773cfe5
interreturn <touchzinginterns@gmail.com>
2025-10-09 14:48:09 +0530

    Update reviewToCSV.js

--- reviewToCSV.js
+++ reviewToCSV.js
@@ -128,4 +128,9 @@ app.post("/fetch-reviews", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
